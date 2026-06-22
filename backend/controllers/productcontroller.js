22e3041cc986053991a97a99d5a3d12770aa54b4
qwertyuii7 <mayankchaudharyetw123@gmail.com>
2026-06-22 12:48:44 +0000

    completed main logic

--- backend/controllers/productcontroller.js
+++ backend/controllers/productcontroller.js
@@ -1,49 +1,67 @@
-const {Product_model} = require("../models/productsSchema");
+const { Product_model } = require("../models/productsSchema");
 
 
-const get_products = async (req ,res ) =>{
-    
-    const limit = req.query.limit || 20;
-    const category = req.query.category;
-    const cursor = req.query.cursor;
+const get_products = async (req, res) => {
+    try {
 
-    const query = {};
+        const limit = req.query.limit || 20;
+        const category = req.query.category;
+        const cursor = req.query.cursor;
 
-    if (category){
-        query.category = category;
+        const query = {};
+
+        if (category) {
+            query.category = category;
+        }
+
+        if (cursor) {
+            const decoded = decodeCursor(cursor);
+            query.$or = [
+                { created_at: { $lt: new Date(decoded.created_at) } },
+                {
+                    created_at: new Date(decoded.created_at),
+                    _id: { $lt: decoded.id }
+                }
+            ]
+        }
+
+
+        const products = await Product_model.find(query).sort({ created_at: -1, _id: -1 }).limit(limit + 1);
+
+        const hasmore = products.length > limit;
+        const pageItems = hasmore ? products.slice(0, limits) : products;
+
+        let nextCursor = null;
+        if (hasmore) {
+            const lastitem = pageItems[pageItems.length - 1];
+            nextCursor = encodeCursor(lastitem.created_id, lastitem._id);
+        }
+
+
+        res.json({
+            data: pageItems,
+            cursor: nextCursor
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            error : "something went wrong for this product"
+        });
+
     }
-
-    if(cursor){
-        const decoded = decodeCursor(cursor);
-        query.$or =[
-            {created_at :{ $lt : new Date(decoded.created_at)}},
-            {
-                created_at: new Date(decoded.created_at),
-                _id : {$lt :decoded.id}
-            }
-        ]
-    }
-
-
-    const  products = await Product_model.find(query).sort({created_at:-1, _id :-1 }).limit(limit+1);
-
-    const hasmore = products.length > limit ;
-    const pageItems = hasmore ? products.slice(0, limits ):products;
-
-    let nextCursor = null ;
-    if ( hasmore ){
-        const lastitem = pageItems[pageItems.length-1 ];
-        nextCursor = encodeCursor(lastitem.created_id , lastitem._id);
-    }
-
-
-
-
-
-
-
-
-
-
 
 }
+
+
+function encodeCursor(created_at, id) {
+  const payload = JSON.stringify({ created_at, id });
+  return Buffer.from(payload).toString('base64');
+}
+
+function decodeCursor(cursor) {
+  const payload = Buffer.from(cursor, 'base64').toString('utf-8');
+  return JSON.parse(payload);
+}
+
+module.exports = { getProducts };
